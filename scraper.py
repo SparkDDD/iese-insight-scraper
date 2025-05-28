b922e0cff10332f1dd6f33041b0fa4f7b4926afe
exed <144458825+SparkDDD@users.noreply.github.com>
2025-05-28 09:07:36 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,7 +20,7 @@ TABLE_NAME = os.getenv("AIRTABLE_TABLE_ID")
 EMAIL_FROM = os.getenv("EMAIL_FROM")
 EMAIL_TO = os.getenv("EMAIL_TO")
 SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
-SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
+SMTP_PORT = int(os.getenv("SMTP_PORT") or 587)
 SMTP_USERNAME = os.getenv("SMTP_USERNAME")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 
